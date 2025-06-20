23de5873cea85a1fb162b89b00cacf9f36bf5e72
2305G6YE <ulmi910@gmail.com>
2025-06-20 09:27:24 +0900

    Complete: 2 questions

---
+++ 05-objectPractice/taskOne.js
@@ -0,0 +1,32 @@
+// Creating an Object
+
+// let user = {
+//     name: "John",
+//     surname: "Smith",
+// };
+// console.log(user);
+
+// user.name = "Pete";
+// console.log(user.name);
+
+// delete user.name;
+// console.log (user);
+
+
+// Check for True/False in Object
+
+// let schedule = {};
+// function isEmpty(obj) {
+//    for (let key in obj) {
+//     return false;
+//     };
+// return true;
+// }
+// console.log(isEmpty(schedule));
+
+// schedule["8.30"] = "get up";
+// console.log(isEmpty(schedule));
+
+
+// Summing object properties
+

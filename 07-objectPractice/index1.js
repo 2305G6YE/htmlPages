affae18dcccbc0b71c6625f16180c1ab95fd9f97
2305G6YE <ulmi910@gmail.com>
2025-07-17 15:13:54 +0900

    ADD: 07-objectPractice section add

---
+++ 07-objectPractice/index1.js
@@ -0,0 +1,25 @@
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let users = [ john, pete, mary ];
+
+// let names = users.map(item => item.name);
+
+// alert( names);
+
+// ------------------------------
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+let usersMapped = users.map(user => ({
+    fullName: `${user.name}${user.surname}`,
+    id: user.id
+}));
+
+alert (usersMapped[0].id);
+alert (usersMapped[0].fullName);

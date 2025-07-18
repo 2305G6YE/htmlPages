8ef3aada3b77591df0e4d8ee130530f070029fca
2305G6YE <ulmi910@gmail.com>
2025-07-18 10:28:45 +0900

    Final exercise logic comprehend

--- 07-objectPractice/index1.js
+++ 07-objectPractice/index1.js
@@ -10,16 +10,67 @@
 
 // ------------------------------
 
-let john = { name: "John", surname: "Smith", id: 1 };
-let pete = { name: "Pete", surname: "Hunt", id: 2 };
-let mary = { name: "Mary", surname: "Key", id: 3 };
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
 
-let users = [ john, pete, mary ];
+// let users = [ john, pete, mary ];
 
-let usersMapped = users.map(user => ({
-    fullName: `${user.name}${user.surname}`,
-    id: user.id
-}));
+// let usersMapped = users.map(user => ({
+//     fullName: `${user.name}${user.surname}`,
+//     id: user.id
+// }));
 
-alert (usersMapped[0].id);
-alert (usersMapped[0].fullName);
+// alert (usersMapped[0].id);
+// alert (usersMapped[0].fullName);
+
+// ---------------------------------
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let arr = [ pete, john, mary ];
+
+// function sortByAge(arr) {
+//     arr.sort ((a,b) => )
+// }
+
+// sortByAge(arr);
+
+// // now: [john, mary, pete]
+// alert(arr[0].name); // John
+// alert(arr[1].name); // Mary
+// alert(arr[2].name); // Pete
+
+// ----------------------------------
+// getAverageAge(arr) {
+//     return users.reduce((prev, user) => prev + user.age,0) / 
+//     users.length;
+// }
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 29 };
+
+// let arr = [ john, pete, mary ];
+
+// alert( getAverageAge(arr) );
+
+// --------------------------------
+
+groupById(arr) {
+    return arr.reduce((obj, value) => {
+        obj[value.id] = value;
+        return obj;
+    })
+}
+
+let users = [
+  {id: 'john', name: "John Smith", age: 20},
+  {id: 'ann', name: "Ann Smith", age: 24},
+  {id: 'pete', name: "Pete Peterson", age: 31},
+];
+
+let usersById = groupById(users);
+
